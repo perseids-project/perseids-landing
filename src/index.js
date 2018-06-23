9d4e49e0556254e86ec2a4db44830679fafe5ae4
zfletch <zfletch2@gmail.com>
2018-06-22 20:13:35 -0400

    move scss files aorund

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './styles/styles.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
