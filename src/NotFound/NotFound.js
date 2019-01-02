ede0a3f66298524e55b024370968b278cebc2950
zfletch <zfletch2@gmail.com>
2019-01-02 14:18:40 -0500

    add 404 to not found page

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import Hero from '../Hero';
 const NotFound = () => (
   <React.Fragment>
     <Hero
-      title="Page Not Found"
+      title="404 Page Not Found"
       text="Looks like you've followed a broken link or entered a URL that doesn't exist on this site."
       background="funerary-plaque"
     />
