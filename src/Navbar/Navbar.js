7ba781126a51df71c487a5ccfdecf9047dd90033
zfletch <zfletch2@gmail.com>
2019-01-04 16:48:27 -0500

    support browsing without js on small screens

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 import { NavbarToggler, Collapse } from 'reactstrap';
 import { PerseidsHeader } from 'perseids-react-components';
-
 import { NavLink } from 'react-router-dom';
+
+const renderCollapse = collapsed => (
+  <Collapse isOpen={!collapsed} navbar>
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/perseids-platform">
+          Perseids Platform
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/digital-editions">
+          Digital Editions
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/libraries-tools">
+          Libraries and Tools
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/contact">
+          Contact Us
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://sites.tufts.edu/perseids/">
+          Blog
+        </a>
+      </li>
+    </ul>
+
+    <ul className="navbar-nav my-2 my-lg-02">
+      <li className="nav-item">
+        <a className="nav-link" href="https://sosol.perseids.org/sosol/signin">
+          Log in
+        </a>
+      </li>
+    </ul>
+  </Collapse>
+);
 
 class Navbar extends Component {
   state = {
@@ -17,47 +56,21 @@ class Navbar extends Component {
     const { collapsed } = this.state;
 
     return (
-      <PerseidsHeader>
-        <NavbarToggler onClick={this.toggleNavbar} aria-label="navigation menu" />
+      <React.Fragment>
 
-        <Collapse isOpen={!collapsed} navbar>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/perseids-platform">
-                Perseids Platform
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/digital-editions">
-                Digital Editions
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/libraries-tools">
-                Libraries and Tools
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">
-                Contact Us
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="https://sites.tufts.edu/perseids/">
-                Blog
-              </a>
-            </li>
-          </ul>
+        <PerseidsHeader>
+          <NavbarToggler onClick={this.toggleNavbar} aria-label="navigation menu" />
 
-          <ul className="navbar-nav my-2 my-lg-02">
-            <li className="nav-item">
-              <a className="nav-link" href="https://sosol.perseids.org/sosol/signin">
-                Log in
-              </a>
-            </li>
-          </ul>
-        </Collapse>
-      </PerseidsHeader>
+          {renderCollapse(collapsed)}
+        </PerseidsHeader>
+
+        <noscript>
+          <div className="container-fluid bg-light d-md-none noscript-fake-navbar">
+            {renderCollapse(false)}
+          </div>
+        </noscript>
+
+      </React.Fragment>
     );
   }
 }
