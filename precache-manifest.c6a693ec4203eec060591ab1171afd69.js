25dd997b8e0710208cc7ecb25dd1dc222d190fcf
zfletch <zfletch2@gmail.com>
2019-01-14 13:46:24 -0500

    Updates

---
+++ precache-manifest.c6a693ec4203eec060591ab1171afd69.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "4619aefc8421e8d604be5518fe04d2ef",
+    "url": "/static/media/sunset.4619aefc.jpg"
+  },
+  {
+    "revision": "c9df228cb2a571296f57",
+    "url": "/static/css/main.9151686f.chunk.css"
+  },
+  {
+    "revision": "452a4e93cb2b83e5284269a130ff25ec",
+    "url": "/static/media/morphsvc.452a4e93.png"
+  },
+  {
+    "revision": "332f85c87aa0648bb1cd",
+    "url": "/static/js/1.332f85c8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "46ad201c4312c9efe35fd0ae0f20ce88",
+    "url": "/static/media/treebank.46ad201c.png"
+  },
+  {
+    "revision": "1b552b641625003462d340f42f88d1a5",
+    "url": "/static/media/frqsc.1b552b64.png"
+  },
+  {
+    "revision": "56ac5fbffa76917e52b34893febe263b",
+    "url": "/static/media/imls.56ac5fbf.png"
+  },
+  {
+    "revision": "ccc1f79fc146793d40568e22e4ca2f27",
+    "url": "/static/media/kress.ccc1f79f.png"
+  },
+  {
+    "revision": "5dc2d375fd156ada680a6af6d60e06f8",
+    "url": "/static/media/mellon.5dc2d375.jpg"
+  },
+  {
+    "revision": "bd084aaeac777b2df78fc46bdde2d64f",
+    "url": "/static/media/tufts.bd084aae.png"
+  },
+  {
+    "revision": "dad23906be0d73c673ba4352ca8f2a0b",
+    "url": "/static/media/columns.dad23906.jpg"
+  },
+  {
+    "revision": "217c13d2ab5129d628a3bb7ffe843a32",
+    "url": "/static/media/construction.217c13d2.jpg"
+  },
+  {
+    "revision": "067ea29109e25bb28bc10d5b7d786359",
+    "url": "/static/media/hermes.067ea291.jpg"
+  },
+  {
+    "revision": "c9df228cb2a571296f57",
+    "url": "/static/js/main.c9df228c.chunk.js"
+  },
+  {
+    "revision": "90ba759c88d187d2477c38f0a0de9104",
+    "url": "/static/media/text.90ba759c.png"
+  },
+  {
+    "revision": "ff369f7e3693fcbe3a8543c94fbd18d8",
+    "url": "/static/media/funerary-plaque.ff369f7e.png"
+  },
+  {
+    "revision": "7f1f7156a5415bc632dfda0b43a2e87d",
+    "url": "/static/media/digmill.7f1f7156.png"
+  },
+  {
+    "revision": "3f5d961f70a9f226ef0bbf9f8a62f954",
+    "url": "/static/media/agldt.3f5d961f.png"
+  },
+  {
+    "revision": "c49db818a1c067e0ee1f798ea46e3b19",
+    "url": "/static/media/bodin.c49db818.png"
+  },
+  {
+    "revision": "163724ee25fbce28f430f0d8b54178f2",
+    "url": "/static/media/community.163724ee.png"
+  },
+  {
+    "revision": "97595473b852d09c92bdab0dedc370e4",
+    "url": "/static/media/manuscript.97595473.png"
+  },
+  {
+    "revision": "9abbf2d25a2a0d2aa25a704b73a7bb08",
+    "url": "/static/media/sharing.9abbf2d2.png"
+  },
+  {
+    "revision": "56297882e02d398827cd22bda0f1d241",
+    "url": "/static/media/code.56297882.png"
+  },
+  {
+    "revision": "0c86748f91d8121ef05c5b27c7ef4d56",
+    "url": "/static/media/demixer.0c86748f.png"
+  },
+  {
+    "revision": "6bd6c910c5beef1321eac4e8a4cfe77e",
+    "url": "/static/media/lexicon.6bd6c910.png"
+  },
+  {
+    "revision": "332f85c87aa0648bb1cd",
+    "url": "/static/css/1.32733b54.chunk.css"
+  },
+  {
+    "revision": "25348109f4ebfbbb7277ec88443ceef3",
+    "url": "/index.html"
+  }
+];
