551560a490e15442fc5c95a09ab6cde55034e48f
zfletch <zfletch2@gmail.com>
2019-02-19 12:40:21 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12a38ba01c1eccef3260e41aadd8064d.js"
+  "/precache-manifest.a9d87d2dde34c189e1a6ceca07fd57ee.js"
 );
 
 workbox.clientsClaim();
