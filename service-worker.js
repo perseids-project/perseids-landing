d6051f70d1007b532cf7aabe68165ffdd6998d44
zfletch <zfletch@users.noreply.github.com>
2020-07-30 17:23:54 +0000

    Deploying to perseids-build from  @ bd840abe3e9115af9b57d2819b283888d6f6bbf1 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5caedbeb38f9f96a8c65ae70682235d.js"
+  "/precache-manifest.678516ca4704ba4f226ba560cc0606ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
