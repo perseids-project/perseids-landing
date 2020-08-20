085cf84623616de35e2dff20fd452ebf2a90d0c0
zfletch <zfletch@users.noreply.github.com>
2020-08-20 15:10:01 +0000

    Deploying to gh-pages from  @ effeaf45801a16c46a88c20d5b624a67b53f85a0 🚀

---
+++ precache-manifest.94981ced8baf98e9912d4e6d9da1eb1a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3b0020be7cfb7aa22932da7ac06aef",
+    "url": "/perseids-landing/index.html"
+  },
+  {
+    "revision": "3dcedded90573cea82e7",
+    "url": "/perseids-landing/static/css/2.f38cb043.chunk.css"
+  },
+  {
+    "revision": "654b6fa6004c02a6f555",
+    "url": "/perseids-landing/static/css/main.f72b3966.chunk.css"
+  },
+  {
+    "revision": "3dcedded90573cea82e7",
+    "url": "/perseids-landing/static/js/2.0f793b66.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/perseids-landing/static/js/2.0f793b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654b6fa6004c02a6f555",
+    "url": "/perseids-landing/static/js/main.87c6c8da.chunk.js"
+  },
+  {
+    "revision": "e5f7eae4ac7efb706a97",
+    "url": "/perseids-landing/static/js/runtime-main.a049a7e3.js"
+  },
+  {
+    "revision": "71ae50b23fa2a2d403a36230b6907f2d",
+    "url": "/perseids-landing/static/media/ancientbirds.71ae50b2.png"
+  },
+  {
+    "revision": "c49db818a1c067e0ee1f798ea46e3b19",
+    "url": "/perseids-landing/static/media/bodin.c49db818.png"
+  },
+  {
+    "revision": "56297882e02d398827cd22bda0f1d241",
+    "url": "/perseids-landing/static/media/code.56297882.png"
+  },
+  {
+    "revision": "dad23906be0d73c673ba4352ca8f2a0b",
+    "url": "/perseids-landing/static/media/columns.dad23906.jpg"
+  },
+  {
+    "revision": "163724ee25fbce28f430f0d8b54178f2",
+    "url": "/perseids-landing/static/media/community.163724ee.png"
+  },
+  {
+    "revision": "217c13d2ab5129d628a3bb7ffe843a32",
+    "url": "/perseids-landing/static/media/construction.217c13d2.jpg"
+  },
+  {
+    "revision": "0c86748f91d8121ef05c5b27c7ef4d56",
+    "url": "/perseids-landing/static/media/demixer.0c86748f.png"
+  },
+  {
+    "revision": "7f1f7156a5415bc632dfda0b43a2e87d",
+    "url": "/perseids-landing/static/media/digmill.7f1f7156.png"
+  },
+  {
+    "revision": "1b552b641625003462d340f42f88d1a5",
+    "url": "/perseids-landing/static/media/frqsc.1b552b64.png"
+  },
+  {
+    "revision": "ff369f7e3693fcbe3a8543c94fbd18d8",
+    "url": "/perseids-landing/static/media/funerary-plaque.ff369f7e.png"
+  },
+  {
+    "revision": "067ea29109e25bb28bc10d5b7d786359",
+    "url": "/perseids-landing/static/media/hermes.067ea291.jpg"
+  },
+  {
+    "revision": "56ac5fbffa76917e52b34893febe263b",
+    "url": "/perseids-landing/static/media/imls.56ac5fbf.png"
+  },
+  {
+    "revision": "ccc1f79fc146793d40568e22e4ca2f27",
+    "url": "/perseids-landing/static/media/kress.ccc1f79f.png"
+  },
+  {
+    "revision": "6bd6c910c5beef1321eac4e8a4cfe77e",
+    "url": "/perseids-landing/static/media/lexicon.6bd6c910.png"
+  },
+  {
+    "revision": "97595473b852d09c92bdab0dedc370e4",
+    "url": "/perseids-landing/static/media/manuscript.97595473.png"
+  },
+  {
+    "revision": "5dc2d375fd156ada680a6af6d60e06f8",
+    "url": "/perseids-landing/static/media/mellon.5dc2d375.jpg"
+  },
+  {
+    "revision": "452a4e93cb2b83e5284269a130ff25ec",
+    "url": "/perseids-landing/static/media/morphsvc.452a4e93.png"
+  },
+  {
+    "revision": "9abbf2d25a2a0d2aa25a704b73a7bb08",
+    "url": "/perseids-landing/static/media/sharing.9abbf2d2.png"
+  },
+  {
+    "revision": "4619aefc8421e8d604be5518fe04d2ef",
+    "url": "/perseids-landing/static/media/sunset.4619aefc.jpg"
+  },
+  {
+    "revision": "90ba759c88d187d2477c38f0a0de9104",
+    "url": "/perseids-landing/static/media/text.90ba759c.png"
+  },
+  {
+    "revision": "46ad201c4312c9efe35fd0ae0f20ce88",
+    "url": "/perseids-landing/static/media/treebank.46ad201c.png"
+  },
+  {
+    "revision": "bd084aaeac777b2df78fc46bdde2d64f",
+    "url": "/perseids-landing/static/media/tufts.bd084aae.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/perseids-landing/precache-manifest.cb393f5bcb5313dfe1f2fc379d84f1ee.js"
+  "/perseids-landing/precache-manifest.94981ced8baf98e9912d4e6d9da1eb1a.js"
 );
 
 self.addEventListener('message', (event) => {
