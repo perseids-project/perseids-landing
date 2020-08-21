bbff0da75725819f4a9ece862f6b147c2801c66f
zfletch <zfletch@users.noreply.github.com>
2020-08-21 00:57:46 +0000

    Deploying to perseids-build from  @ 8b799d9d53036603fd1d30880397a41344b588cb 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaf760f837df833887f88414a3c5008f.js"
+  "/precache-manifest.b9cd757e7330df979aeb72def0a0337c.js"
 );
 
 self.addEventListener('message', (event) => {
